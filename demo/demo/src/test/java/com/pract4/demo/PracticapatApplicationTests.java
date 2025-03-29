69c66b5608ef60210c6df1974e7dc4b3efb07a50
crisstinn <202204379@alu.comillas.edu>
2025-03-29 17:33:02 +0100

    Add files via upload

---
+++ demo/demo/src/test/java/com/pract4/demo/PracticapatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pract4.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticapatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

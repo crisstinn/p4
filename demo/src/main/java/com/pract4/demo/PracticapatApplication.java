c4434b1be92ebcdc1f2da486fc1fe0f60a38ee6b
crisstinn <202204379@alu.comillas.edu>
2025-03-29 17:52:44 +0100

    Add files via upload

---
+++ demo/src/main/java/com/pract4/demo/PracticapatApplication.java
@@ -0,0 +1,13 @@
+package com.pract4.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticapatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticapatApplication.class, args);
+	}
+
+}
